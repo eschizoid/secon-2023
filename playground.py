6cdc3c8ecfc61a622cce715ab5d7f46d1bb74e29
mariano <mariano.gonzalez.mx@gmail.com>
2023-04-25 15:53:10 -0500

    Swap Google Flan-T5-XXL model in favor of Databricks Dolly-V2-12B

--- playground.py
+++ playground.py
@@ -21,20 +21,20 @@ def generate_text(input_prompt: str) -> str:
     )
 
     result = json.loads(response["Body"].read().decode())
-    return result
+    return result[0]["generated_text"]
 
 
 session = boto3.Session()
 sagemaker_runtime = session.client("sagemaker-runtime", region_name=session.region_name)
-endpoint_name = "flan-t5-xxl"
+endpoint_name = "dolly-v2-12b"
 
-st.sidebar.title("Flan-T5 Parameters")
+st.sidebar.title("Dolly-V2 Parameters")
 stop_word = st.sidebar.text_input("Stop word")
 min_length, max_length = st.sidebar.slider("Min/Max length", 0, 500, (0, 100))
 temperature = st.sidebar.slider("Temperature", min_value=0.0, max_value=1.0, value=0.6)
 rep_penalty = st.sidebar.slider("Repetition Penalty", min_value=0.9, max_value=1.2, value=1.0)
 
-st.header("Flan-T5-XXL Playground")
+st.header("Dolly-v2-12B Playground")
 prompt = st.text_area("Enter your prompt here:")
 
 if st.button("Run"):
